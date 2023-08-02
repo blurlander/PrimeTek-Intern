9ef8f781265c246e49f6a70991ba883b51be0e62
blurlander <efezdn@hotmail.com>
2023-08-02 10:35:34 +0300

    axios update

--- movie_app/src/app/page.js
+++ movie_app/src/app/page.js
@@ -6,7 +6,10 @@ import { Tag } from 'primereact/tag';
 import { InputText } from 'primereact/inputtext';
 import './background.css';
 import { Button } from 'primereact/button';
+import { DataView } from 'primereact/dataview';
 import { useRouter } from 'next/navigation'
+import axios from '../../node_modules/axios';
+import { AutoComplete } from 'primereact/autocomplete';
 
 export default function Home() {
 
@@ -16,31 +19,64 @@ export default function Home() {
   const router = useRouter();
 
 
-  const getAllMovies = (pageNumber) => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNWQ1YTRjM2Y2ZGZmNzE1YjlmZGJhODMwOWE0MTZkNSIsInN1YiI6IjY0YzRkYWFkOWI2ZTQ3MDBhZDJhMjBjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIBvlru7khkMdfArYG_8oGEZ5eh4T-im7A85KeJcHyw'
-      }
-    };
+  const getAllMovies = () => {
 
-    let url = 'https://api.themoviedb.org/3/movie/popular'.concat('?page=').concat(pageNumber);
-    fetch(url, options)
-      .then(response => response.json())
-      .then(response => setMovies(response.results));
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/movie/popular',
+        params: {language: 'en-US', page: 1},
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNWQ1YTRjM2Y2ZGZmNzE1YjlmZGJhODMwOWE0MTZkNSIsInN1YiI6IjY0YzRkYWFkOWI2ZTQ3MDBhZDJhMjBjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIBvlru7khkMdfArYG_8oGEZ5eh4T-im7A85KeJcHyw'
+        }
+      };
+      
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+          setMovies([].concat(movies,response.data.results));
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
   }
 
 
 
 
   useEffect(() => {
-    getAllMovies(1);
+    getAllMovies(); 
   }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
     setFilter(search);
+  }
+
+  const itemTemplate = (movie) => {
+    return (
+      <div className="col-9 sm:col-8 lg:col-4 xl:col-4 p-2" key={movie.id} title={movie.title}>
+        <div className="p-4 border-1 surface-border surface-card border-round">
+          <div className="flex flex-wrap align-items-center justify-content-between gap-2">
+            <div className="flex align-items-center gap-2">
+              <span className="font-semibold">{movie.category}</span>
+            </div>
+            <Tag value={movie.release_date}></Tag>
+          </div>
+          <div className="flex flex-column align-items-center gap-3 py-5">
+            <img className="w-6 shadow-2 border-round" src={'https://image.tmdb.org/t/p/w500'.concat(movie.poster_path)} alt={movie.name} />
+            <div className="text-2xl font-bold">{movie.title}</div>
+            <Rating value={Math.round(movie.vote_average / 2)} tooltip={movie.vote_average} readOnly cancel={false}></Rating>
+            <span >
+              <Button label="Details"
+                onClick={() => router.push('/movieDetails/'.concat(movie.id))}
+                severity="info" rounded className='z-1' />
+            </span>
+          </div>
+        </div>
+      </div>
+    )
   }
 
 
@@ -55,7 +91,7 @@ export default function Home() {
           <form onSubmit={(e) => handleSearch(e)}>
             <span className="p-input-icon-left">
               <i className="pi pi-search" />
-              <InputText placeholder="Search" onChange={(e) => setSearch(e.target.value)} className='sm:w-15rem  md:w-20rem lg:w-30rem' />
+              <InputText placeholder="Search" onChange={(e) => setSearch(e.target.value)} className='sm:w-10rem  md:w-20rem lg:w-30rem' />
             </span>
             <Button label="Search" rounded className='ml-2'
               style={{ backgroundImage: "linear-gradient(to right, #4880EC, #019CAD)" }}
@@ -64,35 +100,12 @@ export default function Home() {
         </div>
       </div>
       <br></br>
-      <div className='w-screen grid flex justify-content-center mt-8 z-1'>
-        {movies.map(function (movie) {
-          if (movie.title.toLowerCase().includes(filter.toLowerCase())){
-            return (
-              <div className="col-9 sm:col-8 lg:col-4 xl:col-4 p-2" key={movie.id} title={movie.title}>
-                <div className="p-4 border-1 surface-border surface-card border-round">
-                  <div className="flex flex-wrap align-items-center justify-content-between gap-2">
-                    <div className="flex align-items-center gap-2">
-                      <span className="font-semibold">{movie.category}</span>
-                    </div>
-                    <Tag value={movie.release_date}></Tag>
-                  </div>
-                  <div className="flex flex-column align-items-center gap-3 py-5">
-                    <img className="w-6 shadow-2 border-round" src={'https://image.tmdb.org/t/p/w500'.concat(movie.poster_path)} alt={movie.name} />
-                    <div className="text-2xl font-bold">{movie.title}</div>
-                    <Rating value={Math.round(movie.vote_average / 2)} tooltip={movie.vote_average} readOnly cancel={false}></Rating>
-                    <span >
-                      <Button label="Details" 
-                      onClick={() => router.push('/movieDetails/'.concat(movie.id))}
-                       severity="info" rounded className='z-1'/>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            )
-          }
-
-        })
-        }
+      <div>
+        <div className='w-screen grid flex justify-content-center mt-8 z-1'>
+          <DataView layout="grid" value={movies.filter(function movieFilter(movie) {
+            return movie.title.toLowerCase().includes(filter.toLowerCase());
+          })} itemTemplate={itemTemplate} paginator rows={10} />
+        </div>
       </div>
     </main>
 
